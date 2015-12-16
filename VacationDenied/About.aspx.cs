ffc43a99a12daa762f993ad8a2fdb386365af131
jolive1993 <rockitshipshawty@gmail.com>
2015-12-16 12:09:53 -0600

    connection to request table

--- VacationDenied/About.aspx.cs
+++ VacationDenied/About.aspx.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
-using System.Web;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
@@ -18,15 +17,24 @@ namespace VacationDenied
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            var currentUserId = HttpContext.Current.User.Identity.GetUserId();
-            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            //var currentUserId = HttpContext.Current.User.Identity.GetUserId();
+            //var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
             //var currentUser = manager.FindById(User.Identity.GetUserId());
-            var currentUser = manager.FindByEmail("jbreezy@wtf.com");
-            manager.Delete(currentUser);
+            //var currentUser = manager.FindByEmail("admin@admin.com");
+            //currentUser.LockoutEnabled = false;            
+            //manager.Delete(currentUser);
             //currentUser.firstName = "Cool guy ";
             //manager.Update(currentUser);
+            //Models.VacationDate date = new Models.VacationDate();
+            //date.StartDate = DateTime.Parse("2015-12-12");
+            //date.EndDate = DateTime.Parse("2015-12-13");
+            //date.EmployeeID = currentUserId;
+            //date.Status = "pending";
+            //date.Id = "1";
+            //Models.DataClasses1DataContext poop = new Models.DataClasses1DataContext();
+            //poop.VacationDates.InsertOnSubmit(date);
+            //poop.SubmitChanges();
 
         }
-
     }
 }
