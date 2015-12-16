d936d11205628152f8ec8f86422764a2c4d953d1
jolive1993 <rockitshipshawty@gmail.com>
2015-12-16 17:28:08 -0600

    jbreezy hates calendars

--- VacationDenied/Calendar.aspx.cs
+++ VacationDenied/Calendar.aspx.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace VacationDenied
 {
@@ -11,7 +17,21 @@ namespace VacationDenied
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //var currentUserId = HttpContext.Current.User.Identity.GetUserId();
+            var datess = new List<Models.VacationDate>();
+            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var currentUser = manager.FindByEmail("what@what.ocm");
+            var currentUserId = currentUser.Id;
+            Models.DataClasses1DataContext poop = new Models.DataClasses1DataContext();
+            var dates = poop.GetTable<VacationDenied.Models.VacationDate>();
+            var q =
+            from c in poop.VacationDates
+            where c.EmployeeID == currentUserId
+            select c;
+            foreach (Models.VacationDate c in q)
+            {
+                datess.Add(c);
+            }
         }
     }
 }
